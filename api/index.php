72bab68dcac337bcced59f3cfc71b6367ce11973
Luybomir <volkovvolkov22122002@gmail.com>
2025-09-09 20:24:04 +0300

    readme

--- api/index.php
+++ api/index.php
@@ -8,11 +8,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 $data = json_decode(file_get_contents('php://input'), true);
 
-// // print_r($uri);ы
-// // print_r($data);
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+
 $controller = new UserController();
 
 if ($uri[0] === 'users') {
